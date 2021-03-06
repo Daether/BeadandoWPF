12e2aeac41abf118489d411ce2dd17281d7ee550
Daether <acsmarci54@gmail.com>
2021-03-06 13:45:27 +0100

    Beadando.wpf projekt hozzáadása

---
+++ Beadando/Invoice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beadando
+{
+    class Invoice
+    {
+        //public string OwnerName { get; set; } = "";
+        //public int Balance { get; set; } = 0;
+
+        public string OwnerName
+        {
+
+            get
+            {
+                return $"{OwnerName}";
+            }
+            set { }
+            
+        }
+        public int Balance
+        {
+            
+            get
+            {
+                return Balance;
+            }
+            set { }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Beadando
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Invoice> invoice = new List<Invoice>();
+        public MainWindow()
+        {
+            invoice.Add(new Invoice { OwnerName = "Logan", Balance=0});
+            invoice.Add(new Invoice { OwnerName = "Robert", Balance = 0 });
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
